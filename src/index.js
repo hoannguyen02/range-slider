8e922441d0660d47667878f2449441fa44a87677
Hoan Nguyen <nguyenvanhoan02@gmail.com>
2020-11-12 08:26:20 +0700

    Update for render multiple range filter

--- src/index.js
+++ src/index.js
@@ -1,38 +1,6 @@
 import './index.css';
-let inputTag = null;
-let slider = null;
-let sliderWidth = 0;
-let sliderLeft = 0;
-let pointerWidth = 0;
-let pointerR = null;
-let pointerL = null;
-let activePointer = null;
-let selected = null;
-let scaleEL = null;
-let step = 0;
-let tipL = null;
-let tipR = null;
-let canMove = true;
-let emitChange = true;
-// TODO: Rename values bellow for better understand
-let values = {
-  start: null,
-  end: null,
-};
-let conf = {
-  inputId: '',
-  values: [],
-  set: null,
-  width: null,
-  scale: false,
-  labels: false,
-  tooltip: false,
-  step: null,
-  disabled: false,
-  onChange: null,
-};
 
-let cls = {
+const cls = {
   container: 'rf-container rf-clearfix',
   background: 'rf-bg',
   selected: 'rf-selected',
@@ -42,11 +10,44 @@ let cls = {
   tip: 'rf-tooltip',
 };
 
-for (var i in conf) {
-  if (conf.hasOwnProperty(i)) conf[i] = conf[i];
-}
-
 export function init(config) {
+  let inputTag = null;
+  let slider = null;
+  let sliderWidth = 0;
+  let sliderLeft = 0;
+  let pointerWidth = 0;
+  let pointerR = null;
+  let pointerL = null;
+  let activePointer = null;
+  let selected = null;
+  let scaleEL = null;
+  let step = 0;
+  let tipL = null;
+  let tipR = null;
+  let canMove = true;
+  let emitChange = true;
+  // TODO: Rename values bellow for better understand
+  let values = {
+    start: null,
+    end: null,
+  };
+  let conf = {
+    inputId: '',
+    values: [],
+    set: null,
+    width: null,
+    scale: false,
+    labels: false,
+    tooltip: false,
+    step: null,
+    disabled: false,
+    onChange: null,
+  };
+
+  for (var i in conf) {
+    if (conf.hasOwnProperty(i)) conf[i] = conf[i];
+  }
+
   conf = config;
   if (!config.inputId) {
     console.error('Need to provide element with id to generate range filter');
@@ -65,10 +66,8 @@ export function init(config) {
     console.error(`Missing min or max value for range filter`);
     return;
   }
-  createSlider();
-}
 
-function createSlider() {
+  // Create slider
   slider = createElement('div', cls.container);
   slider.innerHTML = '<div class="rf-bg"></div>';
   selected = createElement('div', cls.selected);
@@ -94,26 +93,18 @@ function createSlider() {
   sliderWidth = slider.clientWidth;
   pointerWidth = pointerL.clientWidth;
 
-  setInitialValues();
-}
-
-function setInitialValues() {
+  // Set values
   disabled(conf.disabled);
-
   conf.values = prepareArrayValues();
-
   values.start = conf.min;
   values.end = conf.max;
-
   if (conf.set && conf.set.length && checkInitial(conf)) {
     var vals = conf.set;
     values.start = conf.values.indexOf(vals[0]);
     values.end = conf.set[1] ? conf.values.indexOf(vals[1]) : null;
   }
-  createScale();
-}
 
-function createScale(resize) {
+  // Create scale
   step = sliderWidth / (conf.values.length - 1);
 
   for (var i = 0, iLen = conf.values.length; i < iLen; i++) {
@@ -131,10 +122,8 @@ function createScale(resize) {
 
     ins.style.marginLeft = (ins.clientWidth / 2) * -1 + 'px';
   }
-  addEvents();
-}
 
-function addEvents() {
+  // Add events
   const pointers = slider.querySelectorAll('.' + cls.pointer),
     pieces = slider.querySelectorAll('span');
 
@@ -149,174 +138,174 @@ function addEvents() {
 
   window.addEventListener('resize', onResize.bind(this));
 
-  setValues();
-}
+  function drag(e) {
+    e.preventDefault();
 
-function drag(e) {
-  e.preventDefault();
+    if (conf.disabled) return;
 
-  if (conf.disabled) return;
+    var dir = e.target.getAttribute('data-dir');
+    if (dir === 'left') activePointer = pointerL;
+    if (dir === 'right') activePointer = pointerR;
+  }
 
-  var dir = e.target.getAttribute('data-dir');
-  if (dir === 'left') activePointer = pointerL;
-  if (dir === 'right') activePointer = pointerR;
-}
+  function move(e) {
+    if (activePointer && !conf.disabled) {
+      var coordX = e.type === 'touchmove' ? e.touches[0].clientX : e.pageX,
+        index = coordX - sliderLeft - pointerWidth / 2;
 
-function move(e) {
-  if (activePointer && !conf.disabled) {
-    var coordX = e.type === 'touchmove' ? e.touches[0].clientX : e.pageX,
-      index = coordX - sliderLeft - pointerWidth / 2;
+      index = Math.round(index / step);
+      if (index <= 0) index = 0;
+      if (index > conf.values.length - 1) index = conf.values.length - 1;
 
-    index = Math.round(index / step);
-    if (index <= 0) index = 0;
-    if (index > conf.values.length - 1) index = conf.values.length - 1;
-
-    // Won't set values and emit if index greater smaller than start or greater than end again
-    if (index === 0 || index === conf.values.length - 1) {
-      if (!canMove && emitChange) {
-        emitChange = false;
-      }
-      canMove = false;
-    } else {
-      emitChange = true;
-      canMove = true;
-    }
-
-    const { start, end } = values;
-    let newStart = start,
-      newEnd = end;
-
-    if (emitChange) {
-      if (activePointer === pointerL) {
-        newStart = index;
-      }
-      if (activePointer === pointerR) {
-        newEnd = index;
-      }
-      // Won't set values and emit if the same values
-      if (newStart === start && newEnd === end) {
-        return;
+      // Won't set values and emit if index greater smaller than start or greater than end again
+      if (index === 0 || index === conf.values.length - 1) {
+        if (!canMove && emitChange) {
+          emitChange = false;
+        }
+        canMove = false;
       } else {
-        values.start = newStart;
-        values.end = newEnd;
+        emitChange = true;
+        canMove = true;
       }
 
-      setValues();
-      onChange();
+      const { start, end } = values;
+      let newStart = start,
+        newEnd = end;
+
+      if (emitChange) {
+        if (activePointer === pointerL) {
+          newStart = index;
+        }
+        if (activePointer === pointerR) {
+          newEnd = index;
+        }
+        // Won't set values and emit if the same values
+        if (newStart === start && newEnd === end) {
+          return;
+        } else {
+          values.start = newStart;
+          values.end = newEnd;
+        }
+
+        setValues();
+        onChange();
+      }
     }
   }
-}
 
-function drop() {
-  activePointer = null;
-}
-
-function onClickPiece(e) {
-  if (conf.disabled) return;
-
-  var idx = Math.round((e.clientX - sliderLeft) / step);
-
-  if (idx > conf.values.length - 1) idx = conf.values.length - 1;
-  if (idx < 0) idx = 0;
-
-  if (idx - values.start <= values.end - idx) {
-    values.start = idx;
-  } else {
-    values.end = idx;
+  function drop() {
+    activePointer = null;
   }
 
-  setValues();
-}
+  function onClickPiece(e) {
+    if (conf.disabled) return;
 
-function setValues() {
-  if (values.start > values.end) values.start = values.end;
+    var idx = Math.round((e.clientX - sliderLeft) / step);
 
-  pointerL.style.left = values['start'] * step - pointerWidth / 2 + 'px';
+    if (idx > conf.values.length - 1) idx = conf.values.length - 1;
+    if (idx < 0) idx = 0;
 
-  if (conf.tooltip) {
-    tipL.innerHTML = conf.values[values.start];
-    tipR.innerHTML = conf.values[values.end];
+    if (idx - values.start <= values.end - idx) {
+      values.start = idx;
+    } else {
+      values.end = idx;
+    }
+
+    setValues();
   }
 
-  inputTag.value = JSON.stringify({
-    start: conf.values[values.start],
-    end: conf.values[values.end],
-  });
+  function setValues() {
+    if (values.start > values.end) values.start = values.end;
 
-  pointerR.style.left = values.end * step - pointerWidth / 2 + 'px';
+    pointerL.style.left = values['start'] * step - pointerWidth / 2 + 'px';
 
-  if (values.end > conf.values.length - 1) values.end = conf.values.length - 1;
-  if (values.start < 0) values.start = 0;
+    if (conf.tooltip) {
+      tipL.innerHTML = conf.values[values.start];
+      tipR.innerHTML = conf.values[values.end];
+    }
 
-  selected.style.width = (values.end - values.start) * step + 'px';
-  selected.style.left = values.start * step + 'px';
-}
+    inputTag.value = JSON.stringify({
+      start: conf.values[values.start],
+      end: conf.values[values.end],
+    });
 
-function onChange() {
-  if (conf.onChange && typeof conf.onChange === 'function') {
-    conf.onChange(JSON.parse(inputTag.value));
-  }
-}
+    pointerR.style.left = values.end * step - pointerWidth / 2 + 'px';
 
-function onResize() {
-  sliderLeft = slider.getBoundingClientRect().left;
-  sliderWidth = slider.clientWidth;
-  updateScale();
-}
+    if (values.end > conf.values.length - 1)
+      values.end = conf.values.length - 1;
+    if (values.start < 0) values.start = 0;
 
-function updateScale() {
-  step = sliderWidth / (conf.values.length - 1);
-
-  const pieces = slider.querySelectorAll('span');
-
-  for (var i = 0, iLen = pieces.length; i < iLen; i++)
-    pieces[i].style.width = step + 'px';
-
-  setValues();
-}
-
-function disabled(disabled) {
-  conf.disabled = disabled;
-  slider.classList[disabled ? 'add' : 'remove']('disabled');
-}
-
-function createElement(el, cls, dataAttr) {
-  var element = document.createElement(el);
-  if (cls) element.className = cls;
-  if (dataAttr && dataAttr.length === 2)
-    element.setAttribute('data-' + dataAttr[0], dataAttr[1]);
-
-  return element;
-}
-
-function createEvents(el, ev, callback) {
-  var events = ev.split(' ');
-
-  for (var i = 0, iLen = events.length; i < iLen; i++)
-    el.addEventListener(events[i], callback);
-}
-
-function prepareArrayValues() {
-  const values = [],
-    range = conf.max - conf.min;
-
-  if (!conf.step) {
-    console.log('No step defined...');
-    return [conf.min, conf.max];
+    selected.style.width = (values.end - values.start) * step + 'px';
+    selected.style.left = values.start * step + 'px';
   }
 
-  for (var i = 0, iLen = range / conf.step; i < iLen; i++)
-    values.push(conf.min + i * conf.step);
+  function onChange() {
+    if (conf.onChange && typeof conf.onChange === 'function') {
+      conf.onChange(JSON.parse(inputTag.value));
+    }
+  }
 
-  if (values.indexOf(conf.max) < 0) values.push(conf.max);
+  function onResize() {
+    sliderLeft = slider.getBoundingClientRect().left;
+    sliderWidth = slider.clientWidth;
+    updateScale();
+  }
 
-  return values;
-}
+  function updateScale() {
+    step = sliderWidth / (conf.values.length - 1);
 
-function checkInitial(conf) {
-  if (!conf.set || conf.set.length < 1) return null;
-  if (conf.values.indexOf(conf.set[0]) < 0) return null;
+    const pieces = slider.querySelectorAll('span');
 
-  if (conf.set.length < 2 || conf.values.indexOf(conf.set[1]) < 0) return null;
-  return true;
+    for (var i = 0, iLen = pieces.length; i < iLen; i++)
+      pieces[i].style.width = step + 'px';
+
+    setValues();
+  }
+
+  function disabled(disabled) {
+    conf.disabled = disabled;
+    slider.classList[disabled ? 'add' : 'remove']('disabled');
+  }
+
+  function createElement(el, cls, dataAttr) {
+    var element = document.createElement(el);
+    if (cls) element.className = cls;
+    if (dataAttr && dataAttr.length === 2)
+      element.setAttribute('data-' + dataAttr[0], dataAttr[1]);
+
+    return element;
+  }
+
+  function createEvents(el, ev, callback) {
+    var events = ev.split(' ');
+
+    for (var i = 0, iLen = events.length; i < iLen; i++)
+      el.addEventListener(events[i], callback);
+  }
+
+  function prepareArrayValues() {
+    const values = [],
+      range = conf.max - conf.min;
+
+    if (!conf.step) {
+      console.log('No step defined...');
+      return [conf.min, conf.max];
+    }
+
+    for (var i = 0, iLen = range / conf.step; i < iLen; i++)
+      values.push(conf.min + i * conf.step);
+
+    if (values.indexOf(conf.max) < 0) values.push(conf.max);
+
+    return values;
+  }
+
+  function checkInitial(conf) {
+    if (!conf.set || conf.set.length < 1) return null;
+    if (conf.values.indexOf(conf.set[0]) < 0) return null;
+
+    if (conf.set.length < 2 || conf.values.indexOf(conf.set[1]) < 0)
+      return null;
+    return true;
+  }
 }
